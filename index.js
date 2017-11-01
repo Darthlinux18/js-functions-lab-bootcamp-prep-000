c7920a04f1b345256000f40ea9edf17aecd372b2
craig sperlazza <craig.sperlazza@gmail.com>
2017-11-01 21:27:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,22 +3,19 @@ function happyHolidays() {
   return('Happy holidays!')
 }
 
-happyHolidays()
 
 function happyHolidaysTo(name) {
   return 'Happy holidays, ${name} !'
 }
 
-happyHolidaysTo('you')
 
 function happyHolidayTo(holiday, name) {
   return 'Happy ${holiday}, ${name}!'
 }
 
-happyHolidayTo("Independence Day", "Craig")
 
 function holidayCountdown(days, holiday) {
   return "It's ${days} until ${holiday}!"
 }
 
-holidayCountdown("5", "Independence Day")
+
