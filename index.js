b1f4d9233b419daccd28723c7c59b76b759d5d17
craig sperlazza <craig.sperlazza@gmail.com>
2017-11-01 21:00:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // write your code below!
+function happyHolidays(){
+  return('Happy holidays!')
+}
+
+happyHolidays()
+
+function happyHolidaysTo(name) {
+  console.log('Happy Holidays,')
+  return  ('Happy holidays, ${name}!')
+}
+
+happyHolidaysTo("Craig")
+
+function happyHolidayTo(holiday, name) {
+  return ('Happy ${holiday}, ${name}!')
+}
+
+happyHolidayTo("Independence Day", "Craig")
+
+function holidayCountdown(days, holiday) {
+  return ("It's ${days} until ${holiday}!")
+}
+
+holidayCountdown("5", "Independence Day")
